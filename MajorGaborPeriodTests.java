11b8db337703a14aaa5cd304e8cab6981e28b50c
Gábor Major <61381495+NotYetTerminal@users.noreply.github.com>
2024-10-25 12:05:24 +0100

    Split methods for readability, added test numbers

--- MajorGaborPeriodTests.java
+++ MajorGaborPeriodTests.java
@@ -11,14 +11,20 @@ import org.junit.jupiter.api.TestFactory;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MajorGaborPeriodTests {
 
+    // Used for printing out test number of each test
+    int[] startingTestNumbers = new int[]{
+            1, 8, 1, 3, 1
+    };
+
     @TestFactory
-    Stream<DynamicTest> testPeriodConstructor() {
+    Stream<DynamicTest> testConstructorValidInput() {
         // Testing data format is: { startHour, endHour }
         int[][] validInputData = new int[][]{
                 {3, 18},
@@ -29,6 +35,25 @@ public class MajorGaborPeriodTests {
                 {2, 24},
                 {7, 9}
         };
+
+        AtomicInteger index = new AtomicInteger(startingTestNumbers[0]);
+        return Arrays.stream(validInputData).map(data -> {
+            int start = data[0];
+            int end = data[1];
+            return DynamicTest.dynamicTest(
+                    "Test Number: " + index.getAndIncrement() + ", Valid Input, Start: " + start + ", End: " + end + ", Expected Result: Create Class",
+                    () -> {
+                        Period testPeriod = new Period(start, end);
+                        assertAll(
+                                () -> assertEquals(start, testPeriod.startHour),
+                                () -> assertEquals(end, testPeriod.endHour)
+                        );
+                    });
+        });
+    }
+
+    @TestFactory
+    Stream<DynamicTest> testConstructorInvalidInput() {
         // Testing data format is: { startHour, endHour }
         int[][] invalidInputData = new int[][]{
                 {-1, 8},
@@ -41,90 +66,70 @@ public class MajorGaborPeriodTests {
                 {12, 12}
         };
 
-        return Stream.concat(
-                // Valid Input test stream
-                Arrays.stream(validInputData).map(data -> {
-                    int start = data[0];
-                    int end = data[1];
-                    return DynamicTest.dynamicTest(
-                            "Valid Input, Start: " + start + ", End: " + end + ", Expected Result: Create Class",
-                            () -> {
-                                Period testPeriod = new Period(start, end);
-                                assertAll(
-                                        () -> assertEquals(start, testPeriod.startHour),
-                                        () -> assertEquals(end, testPeriod.endHour)
-                                );
-                            });
-                }),
-                // Invalid Input test stream
-                Arrays.stream(invalidInputData).map(data -> {
-                    int start = data[0];
-                    int end = data[1];
-                    return DynamicTest.dynamicTest(
-                            "Invalid Input, Start: " + start + ", End: " + end + ", Expected Result: Throw IllegalArgumentException",
-                            () -> assertThrows(
-                                    IllegalArgumentException.class,
-                                    () -> new Period(start, end)
-                            )
-                    );
-                })
-        );
+        AtomicInteger index = new AtomicInteger(startingTestNumbers[1]);
+        return Arrays.stream(invalidInputData).map(data -> {
+            int start = data[0];
+            int end = data[1];
+            return DynamicTest.dynamicTest(
+                    "Test Number: " + index.getAndIncrement() + ", Invalid Input, Start: " + start + ", End: " + end + ", Expected Result: Throw IllegalArgumentException",
+                    () -> assertThrows(
+                            IllegalArgumentException.class,
+                            () -> new Period(start, end)
+                    )
+            );
+        });
     }
 
     @TestFactory
-    Stream<DynamicTest> testPeriodOverlaps() {
-        // Input test stream
+    Stream<DynamicTest> testOverlapsInputs() {
         Period period = new Period(5, 7);
-        Stream<DynamicTest> inputTestsStream = Stream.of(
+
+        AtomicInteger index = new AtomicInteger(startingTestNumbers[2]);
+        return Stream.of(
                 DynamicTest.dynamicTest(
-                        "Valid Input, Expected Result: TRUE",
+                        "Test Number: " + index.getAndIncrement() + ", Valid Input, Expected Result: TRUE",
                         () -> assertTrue(period.overlaps(new Period(6, 8)))
                 ),
                 DynamicTest.dynamicTest(
-                        "Invalid Input, Expected Result: Throw IllegalArgumentException",
+                        "Test Number: " + index.getAndIncrement() + ", Invalid Input, Expected Result: Throw IllegalArgumentException",
                         () -> assertThrows(
                                 IllegalArgumentException.class,
                                 () -> period.overlaps(null)
                         )
                 )
         );
-
-        // Testing data format is:
-        // For outer Period, test all the inner Periods associated with it,
-        // Expected Result is the boolean
-        Map<Period, Map<Period, Boolean>> validOutputData = Map.of(
-                new Period(10, 12), Map.of(
-                        new Period(11, 14), true,
-                        new Period(12, 14), false,
-                        new Period(13, 14), false
-                ),
-                new Period(7, 10), Map.of(
-                        new Period(5, 8), true,
-                        new Period(5, 7), false,
-                        new Period(5, 6), false
-                )
-        );
-        // Valid Output test stream
-        Stream<DynamicTest> validOutputTestsStream = validOutputData.entrySet().stream().flatMap(data -> {
-            Period period_1 = data.getKey();
-            return data.getValue().entrySet().stream().map(inner_data -> {
-                Period period_2 = inner_data.getKey();
-                boolean result = inner_data.getValue();
-                return DynamicTest.dynamicTest(
-                        "Valid Output, Period 1: " + period_1 + ", Period 2: " + period_2 + ", Expected Result: " + result,
-                        () -> assertEquals(
-                                result,
-                                period_1.overlaps(period_2)
-                        )
-                );
-            });
-        });
-
-        return Stream.concat(inputTestsStream, validOutputTestsStream);
     }
 
     @TestFactory
-    Stream<DynamicTest> testPeriodDuration() {
+    Stream<DynamicTest> testOverlapsValidOutput() {
+        Period period_1 = new Period(10, 12);
+
+        // Testing data format is ( Period, result as a boolean )
+        Map<Period, Boolean> validOutputData = Map.of(
+                new Period(11, 14), true,
+                new Period(12, 14), false,
+                new Period(13, 14), false,
+                new Period(5, 11), true,
+                new Period(5, 10), false,
+                new Period(5, 9), false
+        );
+
+        AtomicInteger index = new AtomicInteger(startingTestNumbers[3]);
+        return validOutputData.entrySet().stream().map(data -> {
+            Period period_2 = data.getKey();
+            boolean result = data.getValue();
+            return DynamicTest.dynamicTest(
+                    "Test Number: " + index.getAndIncrement() + ", Valid Output, Period 1: " + period_1 + ", Period 2: " + period_2 + ", Expected Result: " + result,
+                    () -> assertEquals(
+                            result,
+                            period_1.overlaps(period_2)
+                    )
+            );
+        });
+    }
+
+    @TestFactory
+    Stream<DynamicTest> testDurationValidOutput() {
         // Testing data format is: ( Period, expected result of period.duration() )
         Map<Period, Integer> testingData = Map.of(
                 new Period(0, 10), 10,
@@ -133,11 +138,12 @@ public class MajorGaborPeriodTests {
                 new Period(14, 18), 4
         );
 
+        AtomicInteger index = new AtomicInteger(startingTestNumbers[4]);
         return testingData.entrySet().stream().map(data -> {
             Period period = data.getKey();
             int result = data.getValue();
             return DynamicTest.dynamicTest(
-                    "Valid Output, Duration(" + period.startHour + ",  " + period.endHour + "), Expected Result: " + result,
+                    "Test Number: " + index.getAndIncrement() + ", Valid Output, Duration(" + period.startHour + ",  " + period.endHour + "), Expected Result: " + result,
                     () -> assertEquals(
                             result,
                             period.duration()
